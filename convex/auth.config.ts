4b6dc4fcdcab90beea4f628e8efc59aca99eda17
saran772 <saranpreetsinghsabharwal@gmail.com>
2026-05-31 15:14:49 +0530

    fix convex auth config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://enabling-alien-47.clerk.accounts.dev/",
+      domain: "https://fair-troll-85.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],

f6d55977c3381cbaf1a1ee492e0bfadd71327c0b
saran772 <saranpreetsinghsabharwal@gmail.com>
2026-06-01 13:38:40 +0530

    remove lemonSqueezy file

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,7 +15,6 @@ import type {
 } from "convex/server";
 import type * as codeExecutions from "../codeExecutions.js";
 import type * as http from "../http.js";
-import type * as lemonSqueezy from "../lemonSqueezy.js";
 import type * as snippets from "../snippets.js";
 import type * as users from "../users.js";
 
@@ -30,7 +29,6 @@ import type * as users from "../users.js";
 declare const fullApi: ApiFromModules<{
   codeExecutions: typeof codeExecutions;
   http: typeof http;
-  lemonSqueezy: typeof lemonSqueezy;
   snippets: typeof snippets;
   users: typeof users;
 }>;
